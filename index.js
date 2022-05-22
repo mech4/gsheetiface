4da55ffc44fe8bfc700b357be784de96ca16be77
awal <ahmad.setiarto@gmail.com>
2022-05-22 16:49:51 +0700

    update port parameter

--- index.js
+++ index.js
@@ -145,7 +145,7 @@ app.get("/data", async(req, res) => {
 
 })
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, ()=>{
     console.log(`server started on ${port}`);
 });
